26cb6f7f09204ed07ee5f4f1bbdba291475540d4
sabtain-sarwar <sabtain964@gmail.com>
2019-12-22 00:34:27 +0500

    Making files reusable

--- includes/db.php
+++ includes/db.php
@@ -6,7 +6,7 @@ $db['db_password']  = "";
 $db['db_name']      = "cms";
 
 foreach ($db as $key => $value) {
-    define(strtoupper($key) , $value);
+    define(strtoupper($key), $value);
 }
 
 $connection = mysqli_connect(DB_HOST, DB_USER , DB_PASSWORD , DB_NAME);
