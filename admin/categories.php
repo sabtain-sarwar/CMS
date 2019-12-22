b94ef9269eda0f023c8671a512c1513c248582b2
sabtain-sarwar <sabtain964@gmail.com>
2019-12-23 01:44:16 +0500

    U part of CRUD category is completed

--- admin/categories.php
+++ admin/categories.php
@@ -40,6 +40,39 @@
                                 <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                         </form>
+
+                        <?php
+                            // Update the category title
+                            if(isset($_POST['update'])) {
+                                $cat_id = $_GET['edit'];
+                                $cat_title = $_POST['cat_title'];
+                                $query = "UPDATE category SET cat_title = '{$cat_title}' WHERE cat_id = {$cat_id}";
+                                $result = mysqli_query($connection , $query);
+                                if (!$result) {
+                                    die("QUERY FAILED" . mysqli_error($connection));
+                                }
+                            }
+
+                            // Put the click category title to the update category
+                            if(isset($_GET['edit'])) {
+                                $id = $_GET['edit'];
+                                $query = "SELECT * FROM category WHERE cat_id = {$id}";
+                                $result = mysqli_query($connection , $query);
+                                $row = mysqli_fetch_assoc($result);
+                                if (!$result) {
+                                    die("QUERY FAILED" . mysqli_error($connection));
+                                }
+                        ?>
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="cat-title">Edit Category</label>
+                                <input type="text" class="form-control" name="cat_title" value="<?php if(isset($row['cat_title'])) { echo $row['cat_title']; } ?>">
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="update" value="Update Category">
+                            </div>
+                        </form>
+                        <?php } ?>
                     </div>
 
                     <div class="col-xs-6">
@@ -74,6 +107,7 @@
                                     <td><?php echo $row['cat_id'];?></td>
                                     <td><?php echo $row['cat_title'];?></td>
                                     <td><a href="categories.php?delete=<?php echo $row['cat_id'];?>">Delete</a></td>
+                                    <td><a href="categories.php?edit=<?php echo $row['cat_id'];?>">Edit</a></td>
                                 </tr>
                                 <?php } ?>
                             </tbody>
