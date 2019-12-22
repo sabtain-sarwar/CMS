1c734ec0b051ac543f4fc453572853ab215c22c8
sabtain-sarwar <sabtain964@gmail.com>
2019-12-22 13:43:18 +0500

    pulled image data from db

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
             </p>
             <p><span class="glyphicon glyphicon-time"></span> <?php echo $row['post_date']; ?></p>
             <hr>
-            <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+            <img class="img-responsive" src="images/<?php echo $row['post_image']; ?>" alt="">
             <hr>
             <p><?php echo $row['post_content']; ?></p>
             <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
