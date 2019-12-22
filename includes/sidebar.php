0187ed3b4289b8f7318dbb5dd61f698225c05779
sabtain-sarwar <sabtain964@gmail.com>
2019-12-22 15:51:17 +0500

    Added dynamic categories to the sidebar and created a widget file

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -17,42 +17,27 @@
 
     <!-- Blog Categories Well -->
     <div class="well">
+        <?php
+            $query = "SELECT * FROM category";
+            $result = mysqli_query($connection , $query);
+            if (!$result) {
+                die("QUERY FAILED" . mysqli_error($connection));
+            }
+        ?>
         <h4>Blog Categories</h4>
         <div class="row">
             <div class="col-lg-6">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+                    <?php
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo "<li><a href='#'> {$row['cat_title']} </a></li>";
+                        }
+                    ?>
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
-        </div>
-        <!-- /.row -->
+        </div>  <!-- /.row -->
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus
-            laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+    <?php include "includes/widget.php"; ?>
 </div>
