935351a12b9f5a3e9ff28a9306a1871228f91bfc
sabtain-sarwar <sabtain964@gmail.com>
2019-12-23 16:52:28 +0500

    Page based switch condition

---
+++ admin/includes/view_all_posts.php
@@ -0,0 +1,37 @@
+<table class="table table-bordered table-hover">
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Author</th>
+        <th>Title</th>
+        <th>Category</th>
+        <th>Status</th>
+        <th>Image</th>
+        <th>Tags</th>
+        <th>Comments</th>
+        <th>Date</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    $query = "SELECT * FROM posts";
+    $result = mysqli_query($connection , $query);
+    if (!$result) {
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>{$row['post_id']}</td>";
+        echo "<td>{$row['post_author']}</td>";
+        echo "<td>{$row['post_title']}</td>";
+        echo "<td>{$row['post_category_id']}</td>";
+        echo "<td>{$row['post_status']}</td>";
+        echo "<td><img src='../images/{$row['post_image']}' width='100' alt='Image'></td>";
+        echo "<td>{$row['post_tags']}</td>";
+        echo "<td>{$row['post_comment_count']}</td>";
+        echo "<td>{$row['post_date']}</td>";
+        echo "</tr>";
+    }
+    ?>
+    </tbody>
+</table>
--- admin/posts.php
+++ admin/posts.php
@@ -13,45 +13,22 @@
                         Welcome to Admin
                         <small>Subheading</small>
                     </h1>
+                    <?php
+                        if (isset($_GET['source'])) {
+                            $source = $_GET['source'];
+                        } else {
+                            $source = '';
+                        }
 
-                    <table class="table table-bordered table-hover">
-                        <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Author</th>
-                            <th>Title</th>
-                            <th>Category</th>
-                            <th>Status</th>
-                            <th>Image</th>
-                            <th>Tags</th>
-                            <th>Comments</th>
-                            <th>Date</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                                $query = "SELECT * FROM posts";
-                                $result = mysqli_query($connection , $query);
-                                if (!$result) {
-                                    die("QUERY FAILED" . mysqli_error($connection));
-                                }
-                                while($row = mysqli_fetch_assoc($result)) {
-                                    echo "<tr>";
-                                        echo "<td>{$row['post_id']}</td>";
-                                        echo "<td>{$row['post_author']}</td>";
-                                        echo "<td>{$row['post_title']}</td>";
-                                        echo "<td>{$row['post_category_id']}</td>";
-                                        echo "<td>{$row['post_status']}</td>";
-                                        echo "<td><img src='../images/{$row['post_image']}' width='100' alt='Image'></td>";
-                                        echo "<td>{$row['post_tags']}</td>";
-                                        echo "<td>{$row['post_comment_count']}</td>";
-                                        echo "<td>{$row['post_date']}</td>";
-                                    echo "</tr>";
-                                }
-                            ?>
+                        switch ($source) {
+                            case 'a':
+                            echo 'a';
+                            break;
 
-                        </tbody>
-                    </table>
+                            default:
+                                include "includes/view_all_posts.php";
+                        }
+                    ?>
                 </div>
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
