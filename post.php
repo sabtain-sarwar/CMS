373abd41f0f012928b494ec88700d4282e94503b
sabtain-sarwar <sabtain964@gmail.com>
2019-12-27 23:44:41 +0500

    Displaying comments based on approval

--- post.php
+++ post.php
@@ -78,47 +78,29 @@
 
                 <hr>
 
-                <!-- Posted Comments -->
-
                 <!-- Comment -->
+                <?php
+
+                    $query = "SELECT * FROM comments WHERE comment_status = 'Approved' AND comment_post_id = {$post_id} ORDER BY comment_id DESC";
+                    $result = mysqli_query($connection , $query);
+                    confirmQuery($result);
+                    while($row = mysqli_fetch_assoc($result)) {
+                ?>
                 <div class="media">
                     <a class="pull-left" href="#">
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
                     <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
+                        <h4 class="media-heading"><?php echo $row['comment_author']; ?>
+                            <small><?php echo $row['comment_date']; ?></small>
                         </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        <?php echo $row['comment_content']; ?>
                     </div>
+                    <hr>
                 </div>
-
+                <?php } ?>
                 <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
-                        </div>
-                        <!-- End Nested Comment -->
-                    </div>
-                </div>
-
+                <?php // include "includes/nested_comment.php"; ?>
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
@@ -126,8 +108,6 @@
 
         </div><!-- /.row -->
 
-
         <hr>
-
         <!-- Footer -->
         <?php include "includes/footer.php"; ?>
