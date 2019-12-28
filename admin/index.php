efb8b9a3d33ecb6f86c734a2b48fa93b5e8e2d86
sabtain-sarwar <sabtain964@gmail.com>
2019-12-28 20:15:40 +0500

    Created login form and page and validated user query

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,7 @@
                     <div class="col-lg-12">
                         <h1 class="page-header">
                             Welcome to Admin
-                            <small>Subheading</small>
+                            <small><?php echo $_SESSION['username']; ?></small>
                         </h1>
                     </div>
                 </div><!-- /.row -->
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -15,6 +15,22 @@
         </form>
     </div>
 
+    <!-- Loogin -->
+    <div class="well">
+        <h4>Blog Search</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input type="text" class="form-control" name="username" placeholder="Enter username">
+            </div>
+            <div class="input-group">
+                <input type="password" class="form-control" name="password" placeholder="Enter password">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login" type="submit">Submit</button>
+                </span>
+            </div>
+        </form>
+    </div>
+
     <!-- Blog Categories Well -->
     <div class="well">
         <?php
